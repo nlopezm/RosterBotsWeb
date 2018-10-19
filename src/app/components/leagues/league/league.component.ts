ba047909786ad942ca247839c5aa6a2b57a0bb32
nlopezm <nlopezmarini@gmail.com>
2018-10-19 00:14:21 -0300

    Teams by league

--- src/app/components/leagues/league/league.component.ts
+++ src/app/components/leagues/league/league.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LeagueService } from '../../../services/league/league.service';
+import { TeamService } from '../../../services/team/team.service';
+import { League } from '../../../classes/league';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-league',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeagueComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private leagueService: LeagueService,
+    private teamService: TeamService, public snackBar: MatSnackBar) { }
+  leagueId: number;
+  league: League;
 
   ngOnInit() {
+    this.leagueId = this.route.snapshot.params.leagueId;
+    this.leagueService.getLeague(this.leagueId).subscribe((data) => this.league = data as League, () => { });
+  }
+
+  deleteTeam(i: number) {
+    if (confirm('Are you sure to delete ' + this.league.teams[i].name + '?')) {
+      this.teamService.deleteTeam(this.league.id, this.league.teams[i].id).subscribe(
+        () => {
+          this.league.teams.splice(i, 1);
+          this.snackBar.open('The team was removed', '', { duration: 2000 });
+        }, () => { });
+    }
+
+  }
+
+  createTeam() {
+
   }
 
 }
