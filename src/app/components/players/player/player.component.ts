986ef66a68cc4eb72e0ddbd485955127ae7c38b8
nlopezm <nlopezmarini@gmail.com>
2018-10-19 00:51:16 -0300

    Player view

--- src/app/components/players/player/player.component.ts
+++ src/app/components/players/player/player.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../../../services/player/player.service';
+import { Player } from '../../../classes/player';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-player',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  leagueId: number;
+  teamId: number;
+  playerId: number;
+  player: Player;
+  updating = false;
+
+  constructor(private route: ActivatedRoute, private playerService: PlayerService,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.leagueId = this.route.snapshot.params.leagueId;
+    this.teamId = this.route.snapshot.params.teamId;
+    this.playerId = this.route.snapshot.params.playerId;
+    this.playerService.getPlayer(this.leagueId, this.teamId, this.playerId)
+      .subscribe((data) => this.player = data as Player, () => { });
   }
+
+  updatePlayer() {
+    this.updating = true;
+  }
+
+  confirm() {
+    this.updating = false;
+  }
+
 
 }
--- src/app/components/teams/team/team.component.ts
+++ src/app/components/teams/team/team.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TeamService } from '../../../services/team/team.service';
+import { PlayerService } from '../../../services/player/player.service';
+import { MatSnackBar } from '@angular/material';
+import { Team } from '../../../classes/team';
+
 
 @Component({
   selector: 'app-team',
@@ -6,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
+  leagueId: number;
+  teamId: number;
+  team: Team;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public snackBar: MatSnackBar,
+    private playerService: PlayerService, private teamService: TeamService) { }
 
   ngOnInit() {
+    this.leagueId = this.route.snapshot.params.leagueId;
+    this.teamId = this.route.snapshot.params.teamId;
+    this.teamService.getTeam(this.leagueId, this.teamId).subscribe((data) => this.team = data as Team, () => { });
   }
 
 }
